31cba4dd7f2880d1eaeaf1f08bc2772c434e49ce
Bulgun <bulgunya@mail.ru>
2022-12-03 01:53:55 +0300

    add hometask6

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Задайте количество чисел для ввода");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите случайные числа через запятую");
+string[] numbers = Console.ReadLine().Split(',');
+
+// int[] ArrayInt(string[] num)
+// {
+//     int[] arr = Array.ConvertAll(num, int.Parse);
+//     return arr;
+// }
+
+int PositiveNumbers(string[] num)
+{
+    int[] arr = Array.ConvertAll(num, int.Parse);
+    int digit = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) digit++;
+    }
+    return digit;
+}
+
+// void PrintArray(int[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+//     Console.Write(arr[arr.Length - 1]);
+//     Console.WriteLine("]");
+// }
+
+// int[] array = ArrayInt(numbers);
+// PrintArray(array);
+
+int result = PositiveNumbers(numbers);
+Console.WriteLine($"Количество чисел больше нуля равно {result}");
---
+++ Task43/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// k1 * x + b1 = k2 * x + b2
+// k1 * x - (k2 * x) = b2 - b1
+// x (k1 - k2) = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+Console.WriteLine("Введите значения");
+Console.Write("b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double CoordinateX(double ba, double ka, double bc, double kc)
+{
+    double x = (bc - ba) / (ka - kc);
+    x = Math.Round(x, 1);
+    return x;
+}
+
+double СoordinateY(double ka, double xa, double ba)
+{
+    double y = ka * xa + ba;
+    y = Math.Round(y, 1);
+    return y;
+}
+
+double resultX = CoordinateX(b1, k1, b2, k2);
+Console.WriteLine($"Координата X: {resultX}");
+double resultY = СoordinateY(k1, resultX, b1);
+Console.WriteLine($"Координата Y: {resultY}");
+
+if (resultX == resultY) Console.WriteLine($"Координата точки пересечения двух прямых -> {resultX},{resultY}");
+Console.WriteLine("У заданных прямых нет точки пересечения");
